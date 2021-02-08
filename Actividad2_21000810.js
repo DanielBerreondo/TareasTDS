2bc965a8a3c46290fe122be85175400f5254accd
Daniel Berreondo <danielberreondo17@gmail.com>
2021-02-07 19:25:00 -0600

    First Commit

---
+++ Actividad2_21000810.js
@@ -0,0 +1,55 @@
+//noprotect
+
+var mes = prompt("Digite el numero de mes");
+switch(mes){
+  case "1":
+    console.log("Estamos en invierno");
+    break;
+    
+  case "2":
+    console.log("Estamos en invierno");
+    break;
+    
+  case "3":
+    console.log("Estamos en primavera");
+    break;
+    
+  case "4":
+    console.log("Estamos en primavera");
+    break;
+    
+  case "5":
+    console.log("Estamos en primavera");
+    break;
+    
+  case "6":
+    console.log("Estamos en verano");
+    break;
+    
+  case "7":
+    console.log("Estamos en verano");
+    break;
+    
+  case "8":
+    console.log("Estamos en verano");
+    break;
+    
+  case "9":
+    console.log("Estamos en otoño");
+    break;
+    
+  case "10":
+    console.log("Estamos en otoño");
+    break;
+    
+  case "11":
+    console.log("Estamos en otoño");
+    break;
+    
+  case "12":
+    console.log("Estamos en verano");
+    break;
+    
+  default: console.log("No corresponde a ningun mes");
+    
+}
---
+++ extra1_21000810.js
@@ -0,0 +1,4 @@
+//noprotect
+
+var ingreso = prompt("Espacio para escribir");
+console.log("Usted escribio: "+ingreso);
